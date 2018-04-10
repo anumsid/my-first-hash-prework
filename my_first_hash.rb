c5f2dde1b0af33cee0004aeec954b905104d326f
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-10 23:10:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,9 +17,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -27,19 +25,16 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
-                 }
+                }
   # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
 end
-
-shipping_manifest["oil paintings"]
